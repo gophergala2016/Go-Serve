9025825cb778ab28dfe16752b698f71ca3eb9c6e
praveenmenon <pmenon@qwinixtech.com>
2016-01-23 20:20:21 +0530

    [development]adds functionality to create service

---
+++ api/v1/models/service.go
@@ -0,0 +1,18 @@
+package models
+
+type Service struct {
+	User_id     int    `valid:"numeric"`
+	Type        int    `valid:"numeric"`
+	Description string `valid:"alphanum,required"`
+	Experience  int    `valid:"numeric"`
+	Certificate bool
+	Address     string `valid:"alphanum,required"`
+	City        string `valid:"alphanum,required"`
+	State       string `valid:"alphanum,required"`
+	Country     string `valid:"alphanum,required"`
+}
+
+type SuccessServiceMessage struct {
+	Success string
+	Message string
+}
